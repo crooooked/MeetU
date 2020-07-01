63d552862cc5ad70bdf3262a7da8d8078c871d40
FromFire <yuanxiasummer@icloud.com>
2020-07-01 09:32:53 +0800

    ContentCard类：用来显示一个状态 改了main函数用于测试ContentCard

---
+++ client/app/src/main/java/com/example/meetu/ContentCard.java
@@ -0,0 +1,50 @@
+package com.example.meetu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class ContentCard extends ConstraintLayout {
+
+    public ContentCard(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public void initView(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.view_content_card, this, true);
+    }
+
+    //事件源：userhead头像
+    //进入该用户的个人空间
+    public void visitPersonalSpace(View view) {
+
+    }
+
+    //事件源：repost_button + repost_slogan
+    //转发状态
+    public void repost(View view) {
+
+    }
+
+    //事件源：remark_button + remark_slogan
+    //查看该状态的所有评论
+    public void remark(View view) {
+
+    }
+
+    //事件源：like_button + like_slogan
+    //评论状态
+    public void like(View view) {
+
+    }
+
+    //事件源：remark_edit（输入结束后按回车）
+    //评论状态
+    public void submit_remark(View view) {
+
+    }
+
+}
--- client/app/src/main/java/com/example/meetu/MainActivity.java
+++ client/app/src/main/java/com/example/meetu/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.meetu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout layout = findViewById(R.id.test);
+        layout.addView(new ContentCard(this));
     }
 }
